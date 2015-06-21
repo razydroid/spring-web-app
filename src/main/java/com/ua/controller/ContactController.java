2c2e6f83612eb93906f810a1024fe4bb6af8f282
Hari Nair <razydroid@gmail.com>
2015-06-21 17:54:57 -0500

    updated with java servlet api and more tiles view pages such as about and contact us ...added bootstrap to classic jsp as well

--- src/main/java/com/ua/controller/ContactController.java
+++ src/main/java/com/ua/controller/ContactController.java
@@ -1,25 +1,16 @@
-package com.ua.controller;
+/*package com.ua.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class IndexController {
+public class ContactController {
 	
-	@RequestMapping("/home")
-	public String index(){
-		return "index";
-	}
-	
-	@RequestMapping("/about")
-	public String about(){
-		return "about";
-	}
-
 	@RequestMapping("/contact")
 	public String contact(){
 		return "contact";
 	}
 	
 }
+*/
